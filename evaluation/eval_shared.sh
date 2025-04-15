8ca07fc54a736a492c0b3a062ffcb27c9ae26019
honglin <honglin@andrew.cmu.edu>
2025-04-15 16:49:08 -0400

    Trial eval script

--- evaluation/eval_shared.sh
+++ evaluation/eval_shared.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -x
 
 echo $SLURM_JOB_ACCOUNT
 
@@ -11,7 +10,27 @@ module load cuda
 module load nvhpc
 
 cd $BASE
-conda activate $BASE/cuda_conda
+conda activate $BASE/conda_cuda
 
-huggingface-cli scan-cache
+DATASET=loogle
+DATA_DIR=shortdep_qa
+MODEL=meta-llama/Meta-Llama-3.1-8B-Instruct
+PRESS=observed_attention_press
+
+run() {
+  echo "Evaluating press $PRESS with compression ratio $1 and $2-bit quantization"
+  python3 evaluate.py --device auto --dataset $DATASET --data_dir $DATA_DIR \
+    --model $MODEL \
+    --press $PRESS --compression_ratio $1 --quanto_bits $2 &
+}
+
+run 0.0 16
+run 0.5 16
+run 0.6 16
+run 0.75 16
+run 0.8 16
+run 0.85 16
+run 0.875 16
+run 0.9 16
+run 0.0 8
 

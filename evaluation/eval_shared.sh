15aa3046e2393f0a27954b3843d8f14132c2d3e1
honglin <honglin@andrew.cmu.edu>
2025-04-19 15:19:00 -0400

    Update script

--- evaluation/eval_shared.sh
+++ evaluation/eval_shared.sh
@@ -16,55 +16,5 @@ WORKING_DIR=$BASE/kvpress/evaluation
 
 cd $WORKING_DIR
 
-DATASET=loogle
-DATA_DIR=shortdep_qa
-MODEL=meta-llama/Meta-Llama-3.1-8B-Instruct
-PRESS=observed_attention_press
-
-RESULTS_ROOT="/jet/home/$USER/results"
-
-echo "Using HF_HOME=$HF_HOME"
-echo "Cached models"
-huggingface-cli scan-cache
-
-exit 0
-
-mkdir -p $RESULTS_ROOT
-
-run() {
-  DEVICE=$1
-  RATIO=$2
-  QUANT=$3
-
-  QFLAGS=${QUANT:+--quanto_bits $QUANT}
-  QS=${QUANT:+q$QUANT}
-
-  echo "Evaluating press $PRESS with compression ratio $1 ${QUANT:+$QUANT-bit quantization}"
-
-  STDOUT="$RESULTS_ROOT/$PRESS-$1-$QS-stdout.log"
-  STDERR="$RESULTS_ROOT/$PRESS-$1-$QS-stdout.log"
-
-  echo "Piping output to $STDOUT and $STDERR"
-
-  python3 evaluate.py \
-    --device "cuda:$DEVICE" \
-    --dataset $DATASET --data_dir $DATA_DIR \
-    --model $MODEL \
-    --press $PRESS \
-    --compression_ratio $RATIO \
-    $QFLAGS \
-    --save_path $RESULTS_ROOT > $STDOUT > $STDERR &
-}
-
-mkdir -p results
-
-run 0 0.75
-run 1 0.0 4
-run 2 0.8
-run 3 0.2 4
-run 4 0.85
-run 5 0.4 4
-run 6 0.875
-run 7 0.5 4
-
-wait
+export TQDM_DISABLE=1
+python3 multieval.py
---
+++ evaluation/multieval.py
@@ -0,0 +1,68 @@
+import subprocess
+import torch
+import torch.multiprocessing as mp
+import datasets
+from huggingface_hub import snapshot_download
+from evaluate import DATASET_DICT
+
+dataset_id = "ruler"
+dataset_config = "4096"
+model_repo = "meta-llama/Meta-Llama-3.1-8B-Instruct"
+
+global_settings = {
+    "model": model_repo,
+    "dataset": dataset_id,
+    "data_dir": dataset_config,
+    "press_name": "observed_attention",
+}
+
+jobs = [
+    {"compression_ratio": 0.5, "quantization": 8},
+    {"compression_ratio": 0.6, "quantization": 8},
+]
+
+
+def worker_eval(queue, eval_args):
+    device = queue.get()
+    assert "press_name" in eval_args
+
+    eval_args["device"] = device
+
+    args = ["python", "evaluate.py"]
+    for key, value in eval_args.items():
+        args.append(f"--{key}={str(value)}")
+
+    print("Executing:", *args)
+
+    retval = subprocess.run(args)
+    print(retval)
+    queue.put(device)
+
+
+def setup():
+    assert dataset_id in DATASET_DICT
+    ds_builder = datasets.load_dataset_builder(DATASET_DICT[dataset_id], dataset_config)
+    ds_builder.download_and_prepare()
+    snapshot_download(model_repo, repo_type="model")
+
+
+def main():
+    manager = mp.Manager()
+    gpus = torch.cuda.device_count()
+    q = manager.Queue(maxsize=gpus)
+    for i in range(gpus):
+        q.put(f"cuda:{i}")
+
+    processes = []
+    for job in jobs:
+        p = mp.Process(target=worker_eval, args=(q, dict(**global_settings, **job)))
+        p.start()
+        processes.append(p)
+
+    for p in processes:
+        p.join()
+
+
+if __name__ == "__main__":
+    main()
+
